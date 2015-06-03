b80f36de05125f7854d75cf6368069c20d2fc721
anthonyalden <anthonyalden@msn.com>
2015-06-03 13:29:19 -0700

    add edit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,6 +23,25 @@ class ProductsController < ApplicationController
 		end
 		
 	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		# downcase username when edited so it is stored in lowercase
+		#   this makes the username case insensitve
+		@product.product_name = @product.product_name.downcase
+		if @product.update_attributes(product_params)
+			# return to the sellers item index page after update to user info
+			# redirect_to request.referer
+			redirect_to products_path
+		else
+			render "edit"
+		end
+	end
+
 private
  
 	def product_params
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -4,40 +4,68 @@ RSpec.describe ProductsController, type: :controller do
 
 
 
-describe "GET new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-      
-    end
+  describe "GET new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+        
+      end
   end
 
-# describe "GET show" do
-  
-#     it "returns http success code for products show action" do
-#       #product = FactoryGirl.build_stubbed(:product, id: 1)
-# 	    get :show
-# 	    expect(response).to have_http_status(:success)
-#     end
-# end
-
-describe "GET #show" do
-  
-    it "returns http success code for products show action" do
-    product = FactoryGirl.build_stubbed(:product, product_name: "syrup", id: 2)
-    get :show, id: product.id
-    expect(response).to render_template("show")
-    end
-
+  describe "GET #show" do
     
+      it "returns template success code for products show action" do
+      product = FactoryGirl.build_stubbed(:product, product_name: "syrup", id: 2)
+      get :show, id: product.id
+      expect(response).to render_template("show")
+      end
 
-    it "shows the product" do
-      product = FactoryGirl.build(:product, id: 2)
-      get :show, :id => product.id
-      expect(product.product_name).to eq "syrup"
-    end
+      
+
+      it "shows the product" do
+        product = FactoryGirl.build(:product, id: 2)
+        get :show, :id => product.id
+        expect(product.product_name).to eq "syrup"
+      end
+
+      it "shows the product" do
+        product = FactoryGirl.build(:product, id: 2)
+        get :show, :id => product.id
+        expect(product.product_name).to_not  eq "dog"
+      end
   end
 
+  describe "get edit" do
+      
+      it "returns http success" do
+          product = FactoryGirl.build_stubbed(:product, product_name: "syrup", id: 2)
+          get :edit, id: product.id
+          expect(response).to have_http_status(:success)
+      end
+
+      it "returns template success code for products template edit action" do
+      product = FactoryGirl.build_stubbed(:product, product_name: "syrup", id: 2)
+      get :edit, id: product.id
+      expect(response).to render_template("edit")
+      end
+
+     it "edit the product" do
+        product = FactoryGirl.build(:product, id: 2)
+        get :edit, :id => product.id
+        expect(product.product_name).to eq "syrup"
+      end 
+
+      it "edit the product" do
+        product = FactoryGirl.build(:product, id: 2)
+        get :edit, :id => product.id
+        expect(product.product_name).to_not eq "dog"
+      end 
+  end
+
+  
+    
+      
+    
 
 
 end
