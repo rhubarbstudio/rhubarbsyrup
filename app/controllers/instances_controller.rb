72a14f7950336866a9a8b90ed9f7efbedcad0c9d
Charles Dudley <cd@charlesdudley.com>
2015-06-10 16:52:09 -0700

    properly retrieving join table record

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,10 +1,10 @@
 class InstancesController < ApplicationController
 
 	def create
-		@instance =Instance.new
+		@instance = Instance.new
 		@instance.instance_date = Time.now
-		@product_ingredient_id = IngredientProduct.where({product_id: params[:product_id], ingredient_id: params[:ingredient_id]} )
-		@instance.ingredients_products_id = @product_ingredient_id		
+		@ingredient_product = IngredientProduct.where({product_id: params[:product_id], ingredient_id: params[:ingredient_id]}).take
+		@instance.ingredients_products_id = @ingredient_product.id
 		if @instance.save
 			redirect_to product_path
 		else render "create"
