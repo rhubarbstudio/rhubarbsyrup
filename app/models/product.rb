2501cd31ca4a49e673cb6ac6aa8643bd03727eb4
anthonyalden <anthonyalden@msn.com>
2015-06-15 12:02:06 -0700

    working on instance history

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,17 +11,8 @@ class Product < ActiveRecord::Base
 
 	def self.convert_date(val)
 		if val.length > 0
-  			date= Date.parse(val).strftime("%m/%d/%Y")
-  			hours = val[11..12]
-  			minutes = val[14..15]
-  			month = val[5..6]
-  			year = val[2..3]
-  			day = val[8..9]
   			complete_time = Time.parse(val).in_time_zone("Pacific Time (US & Canada)")
   			complete_time = complete_time.strftime("%m/%d/%Y %I:%M %p")
-  			# complete_time = month+"/"+day+"/"+year+" "+hours+":"+minutes+" "+"UTC"
-  			# complete_time = complete_time.in_time_zone("Pacific Time (US & Canada)")
-  			
   		else
   			return ""
   		end
